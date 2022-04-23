95016d27c968e975d53a7139d6d4f45b4c58ad6d
victorhenriqc <vic.1601@outlook.com>
2022-04-23 15:57:54 -0300

    add a lib jwt encoded

---
+++ pages/metrics.tsx
@@ -0,0 +1,23 @@
+import { setUpApiCliente } from '../services/api'
+import { withSSRAuth } from '../utils/withSSRAuth'
+
+export default function DashBoard(){
+   
+    
+    return (
+        <>
+            <h1>Metricas</h1>
+        </>
+    )
+}
+
+export const getServerSideProps = withSSRAuth(async (ctx) => {
+    const apiClient = setUpApiCliente(ctx)
+    const response = await apiClient.get('/me')
+        
+    console.log(response);
+     
+    return {
+        props: {}
+    }
+})
